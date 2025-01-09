1c81205353df116df6e9835bf798f31daeffa5ba
Cryogen & Cryptok <84856237+CryoWebToken@users.noreply.github.com>
2025-01-09 04:49:57 -0500

    added information to homepage (#1)

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Web3Provider } from "./contex/web3Context";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createBrowserHistory } from 'history';
 
+
+export const history = createBrowserHistory({
+    basename: process.env.PUBLIC_URL
+});
 const theme = createTheme({
   palette: {
     primary: {
       light: "#757ce8",
-      main: "#3f50b5",
+      main: "#000000",
       dark: "#002884",
       contrastText: "#fff",
     },
@@ -28,6 +33,8 @@ ReactDOM.render(
     <ThemeProvider theme={theme}>
       <Web3Provider>
         <App />
+        <text><center><br></br>Use CryoBit to memorialize your loved ones on the block-chain forever. <br></br>
+          Each Cryobit is written directly to the block-chain and is irreversible.</center></text>
       </Web3Provider>
     </ThemeProvider>
   </BrowserRouter>,
